6369c9900eb43680a70e36dd47ffe39306edb4d5
Razvan-Nanu <razvannanu19@gmail.com>
2021-01-20 20:14:08 -0800

    Initial commit

---
+++ Basic-Java-Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,32 @@
+var Y="Alert Message!"; // Giving variabl Y a string
+     
+alert(Y); //Having the alert button call variable Y
+
+
+var X ="Raz said,\"Test Complete.\""; //Gave variable X a string 
+                                    // Testing the backslash
+
+document.write(X); //calling variable X to test
+
+document.write("<br>"); // break between elements
+
+var Z ="This is a "
++ "test to see "
++ "if i did this right." //testing concolate
+
+document.write(Z); //calling variable Z
+
+document.write("<br>");
+
+var family ="Nanu\'s", Dad="Sebastian", Brother ="Ryan"; // Testing out how to give multiple variables a value
+var family =family.fontcolor("red");
+var Dad =Dad.fontcolor("blue");
+var Brother =Brother.fontcolor("green"); //Gave all variables a font color 
+
+document.write(family);
+document.write(Dad);
+document.write(Brother); //Testing out to see if the variables have a font color
+
+document.write("<br>");
+
+document.write(5+8); //Testing the expression
---
+++ README.md
@@ -0,0 +1,2 @@
+# Javascript
+ All my JavaScript projects
